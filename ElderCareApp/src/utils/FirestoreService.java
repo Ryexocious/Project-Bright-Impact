a28759a9cb6ef502d3ca31e5145bfb3935020e27
Adnan-115 <sadman.saad2003@gmail.com>
2025-07-28 19:50:29 +0600

    Added gitignore and basic project structure and initial firebase initialization

---
+++ ElderCareApp/src/utils/FirestoreService.java
@@ -0,0 +1,39 @@
+package utils;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.cloud.FirestoreClient;
+import com.google.cloud.firestore.Firestore;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class FirestoreService {
+
+    private static Firestore db;
+
+    public static Firestore getFirestore() {
+        if (db == null) {
+            try {
+                // Use relative path to avoid hardcoding machine-specific paths
+                String relativePath = "data/firebase/eldercareapp-d9ea0-firebase-adminsdk-fbsvc-cf1472bd91.json";
+                File file = new File(relativePath);
+                System.out.println("Using Firebase credentials from: " + file.getAbsolutePath());
+
+                FileInputStream serviceAccount = new FileInputStream(file);
+
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
+
+                FirebaseApp.initializeApp(options);
+                db = FirestoreClient.getFirestore();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return db;
+    }
+}
